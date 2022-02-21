f340ee89aff5607ff527e5e42ed2034f47424595
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-21 15:30:21 +0600

    map, forEach, filter, find

---
+++ problem-answer7_5.js
@@ -0,0 +1,27 @@
+/* 
+    7.5 [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+*/
+
+// map() method
+/* 
+   -- It calls the function for every element of an array and it returns  all of the function results in a new array.
+*/
+
+// forEach() method:
+/* 
+    --foreach():It calls one function for each element of an array but it does not return anything else.
+*/
+
+// filer() method:
+/* 
+    --filter()Loop through in an array
+    --If conditions satisfy these elements then that filter returns those elements  in a new array.
+    --If the condition is not satisfied then filter() returns an empty array [ ].
+*/
+
+// find() method:
+/* 
+    --find() method loop through in an array
+    --If it finds a specific element then it returns the match first specific  elements.
+    --If not found then it shows the result undefined
+*/
