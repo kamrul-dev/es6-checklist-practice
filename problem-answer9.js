ac9a4403752646e5e2a544ad7eda12265c7186c1
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-21 15:58:41 +0600

    9. Destructuring Array to third elements in three variable

---
+++ problem-answer9.js
@@ -0,0 +1,7 @@
+/* 
+    9. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+*/
+
+const cars = ['BMW', 'Tesla', 'Lamborgini', 'Toyota', 'Tata'];
+const [, , three] = cars;
+console.log(three);
